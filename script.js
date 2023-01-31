cece8d785ac6bee618c93079383022fafbf54a3f
Argam <argam.markosyan1994@gmail.com>
2023-01-31 15:56:02 +0400

    add ball overlap calculation

--- script.js
+++ script.js
@@ -2,37 +2,49 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const radius = 50;
 let isStop = false;
 
-function Ball(x, y, size = radius, color = "green") {
+function Ball({ id, x, y, speed = 5, size = 50, color = "green" }) {
+  this.id = id;
   this.x = x;
   this.y = y;
-  this.xStep = Math.round(Math.random() * 5 + 5);
-  this.yStep = Math.round(Math.random() * 5 + 5);
+  this.xStep = Math.round(Math.random() * speed + speed);
+  this.yStep = Math.round(Math.random() * speed + speed);
+  this.speed = speed;
   this.size = size;
   this.color = color;
 }
 
-const ball1 = new Ball(50, 50, 50, "orange");
-const ball2 = new Ball(500, 500, 30);
-const ballsArr = [ball1, ball2];
+const ball1 = new Ball({ id: 1, x: 100, y: 100, speed: 2, size: 100, color: "orange" });
+const ball2 = new Ball({ id: 2, x: 100, y: 500, speed: 2, size: 100 });
+const ball3 = new Ball({ id: 3, x: 200, y: 300, speed: 2, size: 100, color: "blue" });
+const ballsArr = [ball1, ball2, ball3];
 
-function f() {
+function run() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ballsArr.forEach((item) => {
-    item.x = item.x + item.xStep;
-    item.y = item.y + item.yStep;
+    item.x += item.xStep;
+    item.y += item.yStep;
 
-    if (item.x >= canvas.width - radius || item.x <= radius) {
-      item.x += item.xStep < 0 ? radius - item.x : -(radius + item.x - canvas.width);
-      item.xStep = Math.round(Math.random() * 5 + 5) * (item.xStep > 0 ? -1 : 1);
+    if (item.x >= canvas.width - item.size || item.x <= item.size) {
+      item.x -= item.xStep;
+      item.xStep = Math.round(Math.random() * item.speed + item.speed) * (item.xStep > 0 ? -1 : 1);
     }
-    if (item.y >= canvas.height - radius || item.y <= radius) {
-      item.y += item.yStep < 0 ? radius - item.y : -(radius + item.y - canvas.height);
-      item.yStep = Math.round(Math.random() * 5 + 5) * (item.yStep > 0 ? -1 : 1);
+    if (item.y >= canvas.height - item.size || item.y <= item.size) {
+      item.y -= item.yStep;
+      item.yStep = Math.round(Math.random() * item.speed + item.speed) * (item.yStep > 0 ? -1 : 1);
     }
+
+    ballsArr.forEach((item2) => {
+      const d = Math.sqrt(Math.pow(item2.x - item.x, 2) + Math.pow(item2.y - item.y, 2)) <= item2.size + item.size;
+      if (item.id !== item2.id && d) {
+        item.x -= item.xStep;
+        item.y -= item.yStep;
+        item.xStep = Math.round(Math.random() * item.speed + item.speed) * (item.xStep > 0 ? -1 : 1);
+        item.yStep = Math.round(Math.random() * item.speed + item.speed) * (item.yStep > 0 ? -1 : 1);
+      }
+    });
 
     ctx.beginPath();
     ctx.arc(item.x, item.y, item.size, 0, 2 * Math.PI);
@@ -40,12 +52,12 @@ function f() {
     ctx.fill();
   });
 
-  !isStop && requestAnimationFrame(f);
+  !isStop && requestAnimationFrame(run);
 }
 
-requestAnimationFrame(f);
+requestAnimationFrame(run);
 
 function stop() {
   isStop = !isStop;
-  !isStop && requestAnimationFrame(f);
+  !isStop && requestAnimationFrame(run);
 }
